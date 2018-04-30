dac55952c64521ba1add69d878ff666700980c97
vithursant <vithursan.thangarasa@gmail.com>
2018-04-30 14:28:53 -0400

    Add support to configure availability zone for p3 GPU instances

--- variables.tf
+++ variables.tf
@@ -13,6 +13,12 @@ variable "my_region" {
   description = "The AWS region to deploy into (i.e. us-east-1)"
 }
 
+variable "avail_zone" {
+  type    = "string"
+  default = "us-east-2a"
+  description = "The AWS availability zone location within the selected region (i.e. us-east-2a)."
+}
+
 variable "my_ip" {
   type    = "string"
   default = "161.69.22.122/32"
@@ -37,17 +43,14 @@ variable "ssh-key-dir" {
 variable "instance_type" {
   type    = "string"
   default = "p2.xlarge"
-  description = "The instance type to provision the instances from (i.e.
-p2.xlarge)."
+  description = "The instance type to provision the instances from (i.e. p2.xlarge)."
 
 }
 
 variable "spot_price" {
   type    = "string"
   default = "0.30"
-  description = "The maximum hourly price (bid) you are willing to pay for the
-specified instance, i.e. 0.10. This price should not be below AWS' minimum spot
-price for the instance based on the region."
+  description = "The maximum hourly price (bid) you are willing to pay for the specified instance, i.e. 0.10. This price should not be below AWS' minimum spot price for the instance based on the region."
 }
 
 variable "ebs_volume_size" {
@@ -59,11 +62,11 @@ variable "ebs_volume_size" {
 variable "ami_id" {
   type    = "string"
   default = "ami-4c5c6e29" # Default AWS Deep Learning AMI (Ubuntu)
-  description = "The AMI ID to use for each instance."
+  description = "The AMI ID to use for each instance. The AMI ID will be different depending on the region, even though the name is the same."
 }
 
 variable "num_instances" {
   type    = "string"
-  default = "2"
+  default = "1"
   description = "The number of AWS EC2 instances to provision."
 }
